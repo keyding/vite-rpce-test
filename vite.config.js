c5b10a78edff36fa1ab1de9fc92696b1471de284
Gniiid <tita0x00@gmail.com>
2022-04-14 10:04:59 +0800

    feat: add icons

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import vue from "@vitejs/plugin-vue";
 
 import { chromeExtension } from "rollup-plugin-chrome-extension";
 
-import manifest from "./manifest.json";
+import manifest from "./src/manifest.json";
 import { name, version } from "./package.json";
 
 Object.assign(manifest, { name, version });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), chromeExtension({ manifest })],
+  plugins: [
+    vue(), 
+    chromeExtension({ 
+      manifest, 
+      contentScripts: { 
+        preambleCode:  false
+      }
+    })
+  ]
 });
